3d49b7a37cf1cf88f82eb5f0a8b79c55190d0b82
Marcus-Jon <47177527+Marcus-Jon@users.noreply.github.com>
2019-05-15 15:25:47 +0100

    Update and rename 6_zigzag_conversion_wip.py to 6_zigzag_conversion.py

--- solutions/6_zigzag_conversion.py
+++ solutions/6_zigzag_conversion.py
@@ -2,17 +2,18 @@ class Solution(object):
     def convert(self, string, n):
         
         s = map(str, str(string))
-        s.pop(len(string) - 1)
-        print s
-        print n
+        #s.pop(len(string) - 1)
+        
+        if len(s) == 1:
+            return s[0]
         
         a = []
-        for i in range(0, n):
-            a.append(['-'] * (len(s) / 2))
+        b = []
         
-        x = 0
-        i = 0
-        j = 0
+        for i in range(0, n):
+            a.append([''] * len(s))
+                    
+        x, i, j = 0, 0, 0
         while i != len(a[0]) and x != len(s):
             while j != n and x != len(s):
                 a[j][i] = s[x]
@@ -26,7 +27,12 @@ class Solution(object):
                 x += 1
                 i += 1
             j = 0
-                
-        print a[0]
-        print a[1]
-        print a[2]
+
+        for i in range(0, len(a)):
+            for j in range(0, len(a[0])):
+                if a[i][j] != '':
+                    b.append(a[i][j])
+
+        c = ''.join(b)
+        
+        return c
