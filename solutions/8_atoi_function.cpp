0024e5628344441f9b39b4350fa297f1fa789312
Marcus-Jon <47177527+Marcus-Jon@users.noreply.github.com>
2019-08-26 14:00:46 +0100

    Update 8_atoi_function.cpp

--- solutions/8_atoi_function.cpp
+++ solutions/8_atoi_function.cpp
@@ -3,12 +3,26 @@ public:
     int myAtoi(string str) {
         cout << str << endl;
         cout << str.length() << endl;
-
-        int x = 0;
+        
+        if(str = '')
+        {
+            return 0;
+        }
+        
+        string num_str;
         for(int i = 0; i < str.length(); i++)
         {
+            if(str[i] >= '0' && str[i] <= '9')
+            {
+                num_str += str[i];
+            }
+        }
+        
+        int x = 0;
+        for(int i = 0; i < num_str.length(); i++)
+        {
             x *= 10;
-            x = x + (str[i] - '0');
+            x = x + (num_str[i] - '0');
 
         }
         return x;
